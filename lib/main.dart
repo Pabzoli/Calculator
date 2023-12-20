e55d79afbc4bbf6bd0b3a607e237768cf61a0148
Praise Akorede <2012.104222@stu.plpsd.mb.ca>
2023-12-20 14:14:42 -0600

    just need to refine 'UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
 
 void main() {
   runApp(CalculatorApp());
@@ -8,198 +10,174 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.black,
-        scaffoldBackgroundColor: Color(0xFF1F1F1F),
+      title: 'Simple Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CalculatorScreen(),
+      home: CalculatorHomePage(),
     );
   }
 }
 
-class CalculatorScreen extends StatefulWidget {
+class CalculatorHomePage extends StatefulWidget {
   @override
-  _CalculatorScreenState createState() => _CalculatorScreenState();
+  _CalculatorHomePageState createState() => _CalculatorHomePageState();
 }
 
-class _CalculatorScreenState extends State<CalculatorScreen> {
-  String output = "";
-  String _expression = "";
-  double num1 = 0.0;
-  double num2 = 0.0;
-  String operand = "";
+class _CalculatorHomePageState extends State<CalculatorHomePage> {
+  TextEditingController _inputController = TextEditingController();
+  double firstNumber = 0;
+  double secondNumber = 0;
+  String currentOperation = '';
+  double result = 0;
 
-  void buttonPressed(String buttonText) {
+  @override
+  void dispose() {
+    _inputController.dispose();
+    super.dispose();
+  }
+
+  void _onNumberClick(String value) {
     setState(() {
-      if (buttonText == "C") {
-        output = "";
-        _expression = "";
-        num1 = 0.0;
-        num2 = 0.0;
-        operand = "";
-      } else if (buttonText == "+" ||
-          buttonText == "-" ||
-          buttonText == "*" ||
-          buttonText == "/") {
-        if (output.isNotEmpty) {
-          if (operand != "") {
-            num2 = double.parse(output);
-            calculateResult();
-            operand = buttonText;
-            _expression = output + " $buttonText";
-            output = "";
-          } else {
-            num1 = double.parse(output);
-            operand = buttonText;
-            _expression += " $buttonText";
-            output = "";
-          }
-        }
-      } else if (buttonText == "=") {
-        if (operand != "" && output.isNotEmpty) {
-          num2 = double.parse(output);
-          calculateResult();
-          _expression += " =";
-          output = num1.toString();
-          num1 = 0.0;
-          num2 = 0.0;
-          operand = "";
-        }
-      } else {
-        output += buttonText;
-        _expression += buttonText;
-      }
+      _inputController.text += value;
     });
   }
 
-  void calculateResult() {
-    switch (operand) {
-      case "+":
-        output = (num1 + num2).toString();
-        break;
-      case "-":
-        output = (num1 - num2).toString();
-        break;
-      case "*":
-        output = (num1 * num2).toString();
-        break;
-      case "/":
-        if (num2 != 0) {
-          output = (num1 / num2).toString();
-        } else {
-          output = "Error";
-        }
-        break;
-      default:
-        break;
-    }
-    _expression += " ${num2.toString()}";
+  void _onOperationClick(String operation) {
+    setState(() {
+      firstNumber = double.parse(_inputController.text);
+      _inputController.text = '';
+      currentOperation = operation;
+    });
   }
 
-  Widget buildButton(String buttonText, Color buttonColor) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: ElevatedButton(
-          onPressed: () => buttonPressed(buttonText),
-          style: ElevatedButton.styleFrom(
-            primary: buttonColor,
-            padding: EdgeInsets.all(24.0),
-          ),
-          child: Text(
-            buttonText,
-            style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
-    );
+  void _calculateResult() {
+  setState(() {
+    String expression = _inputController.text;
+    expression = expression.replaceAll('×', '*').replaceAll('÷', '/');
+    Parser p = Parser();
+    Expression exp = p.parse(expression);
+    ContextModel cm = ContextModel();
+    try {
+      result = exp.evaluate(EvaluationType.REAL, cm);
+      _inputController.text = result.toString();
+    } catch (e) {
+      _inputController.text = 'Error';
+    }
+    currentOperation = '';
+  });
+}
+
+
+  void _clearAll() {
+    setState(() {
+      _inputController.text = '';
+      firstNumber = 0;
+      secondNumber = 0;
+      currentOperation = '';
+      result = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculator'),
+        title: Text('Simple Calculator'),
       ),
       body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 90.0),
-            child: Text(
-              _expression,
-              style: TextStyle(fontSize: 24.0, color: Colors.white),
-            ),
-          ),
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            child: Text(
-              output,
-              style: TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
+        children: [
           Expanded(
             child: Container(
-              color: Color(0xFF1F1F1F),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      buildButton("7", Color(0xFF292929)),
-                      buildButton("8", Color(0xFF292929)),
-                      buildButton("9", Color(0xFF292929)),
-                      buildButton("/", Colors.orange),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      buildButton("4", Color(0xFF292929)),
-                      buildButton("5", Color(0xFF292929)),
-                      buildButton("6", Color(0xFF292929)),
-                      buildButton("*", Colors.orange),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      buildButton("1", Color(0xFF292929)),
-                      buildButton("2", Color(0xFF292929)),
-                      buildButton("3", Color(0xFF292929)),
-                      buildButton("-", Colors.orange),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      buildButton(".", Color(0xFF292929)),
-                      buildButton("0", Color(0xFF292929)),
-                      buildButton("00", Color(0xFF292929)),
-                      buildButton("+", Colors.orange),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      buildButton("C", Color(0xFF292929)),
-                      buildButton("=", Colors.blue),
-                    ],
-                  ),
-                ],
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _inputController.text,
+                style: TextStyle(fontSize: 40),
               ),
             ),
           ),
+          SizedBox(height: 16),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                buildButtonRow(['7', '8', '9', '÷']),
+                buildButtonRow(['4', '5', '6', '×']),
+                buildButtonRow(['1', '2', '3', '-']),
+                buildButtonRow(['0', '.', '=', '+']),
+              ],
+            ),
+          ),
+          SizedBox(height: 8),
+          _buildClearButton(),
         ],
       ),
     );
   }
-}
 
+  Widget buildButtonRow(List<String> labels) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: labels.map((label) {
+          if (label != '=') {
+            return Expanded(
+              child: ElevatedButton(
+                onPressed: () => _onNumberClick(label),
+                child: Text(
+                  label,
+                  style: TextStyle(fontSize: 24),
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(24),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return Expanded(
+              child: ElevatedButton(
+                onPressed: _calculateResult,
+                child: Text(
+                  label,
+                  style: TextStyle(fontSize: 24),
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(24),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  primary: Colors.green,
+                ),
+              ),
+            );
+          }
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _buildClearButton() {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: _clearAll,
+        child: Text(
+          'C',
+          style: TextStyle(fontSize: 24),
+        ),
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(24),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+          primary: Colors.red,
+        ),
+      ),
+    );
+  }
+}
